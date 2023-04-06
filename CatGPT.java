9b26bd13f16130d7f787e214a6e8cedfd987f6a3
hjuliasun <98607170+hjuliasun@users.noreply.github.com>
2023-04-06 11:48:43 -0400

    creating new class CatGPT

---
+++ CatGPT.java
@@ -0,0 +1,23 @@
+public class CatGPT implements Contract{
+
+    private 
+
+
+    void grab(String item){
+        this.
+
+
+    }
+    String drop(String item{
+
+    }
+    void examine(String item);
+    void use(String item);
+    boolean walk(String direction);
+    boolean fly(int x, int y);
+    Number shrink();
+    Number grow();
+    void rest();
+    void undo();
+    
+}
